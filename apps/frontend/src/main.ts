f8b507d32ce9f6ce89fff64b4e3260b6c90d4ee4
Kamal Ez-Zoubir <kez-zoub@student.1337.ma>
2025-10-12 15:32:52 +0100

    init

---
+++ apps/frontend/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+  	<h1 class="text-3xl font-bold underline"> this is a tester header</h1>
+  </div>
+`
+
